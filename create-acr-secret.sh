db2ccf8dc375944419040539c6a36049df269cc6
“saipavan.suraparaju” <saipavan.suraparaju@gmail.com>
2025-08-23 23:06:43 +0530

    updated .sh file

--- create-acr-secret.sh
+++ create-acr-secret.sh
@@ -3,6 +3,6 @@
 set -e
 
 # Create new secret
-kubectl create secret docker-registry "${SECRET_NAME}" --docker-server="${DOCKER_SERVER}" --docker-username="${DOCKER_USERNAME}" --docker-password="${DOCKER_PASSWORD}" --docker-email="${DOCKER_EMAIL}" -n "${NAMESPACE}"
+kubectl create secret docker-registry "$SECRET_NAME" --docker-server="$DOCKER_SERVER" --docker-username="$DOCKER_USERNAME" --docker-password="$DOCKER_PASSWORD" --docker-email="$DOCKER_EMAIL" -n "$NAMESPACE"
 
-kubectl get secret "${SECRET_NAME}" -n "${NAMESPACE}"
+kubectl get secret "$SECRET_NAME" -n "$NAMESPACE"
