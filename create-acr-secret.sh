2a79572224e9db2582c92f314dccf315669e87b2
“saipavan.suraparaju” <saipavan.suraparaju@gmail.com>
2025-08-23 23:24:09 +0530

    updated .sh file

--- create-acr-secret.sh
+++ create-acr-secret.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+echo "SECRET_NAME=$SECRET_NAME"
+echo "DOCKER_SERVER=$DOCKER_SERVER"
+echo "DOCKER_USERNAME=$DOCKER_USERNAME"
+echo "DOCKER_PASSWORD=$DOCKER_PASSWORD"
+echo "DOCKER_EMAIL=$DOCKER_EMAIL"
+
+
 # Create new secret
 kubectl create secret docker-registry "$SECRET_NAME" --docker-server="$DOCKER_SERVER" --docker-username="$DOCKER_USERNAME" --docker-password="$DOCKER_PASSWORD" --docker-email="$DOCKER_EMAIL" -n "$NAMESPACE"
 
