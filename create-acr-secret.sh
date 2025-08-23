1e1fbf1765801947a0b73160e7c3d1fbf675eb5f
“saipavan.suraparaju” <saipavan.suraparaju@gmail.com>
2025-08-23 20:39:47 +0530

    updated the pipeline.yml

---
+++ create-acr-secret.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SECRET_NAME=${SECRET_NAME}
+NAMESPACE=${NAMESPACE}
+DOCKER_SERVER=${DOCKER_SERVER}
+DOCKER_USERNAME=${DOCKER_USERNAME}
+DOCKER_PASSWORD=${DOCKER_PASSWORD}
+DOCKER_EMAIL=${DOCKER_EMAIL} 
+
+# Create new secret
+kubectl create secret docker-registry $SECRET_NAME \
+  --docker-server=$DOCKER_SERVER \
+  --docker-username=$DOCKER_USERNAME \
+  --docker-password=$DOCKER_PASSWORD \
+  --docker-email=$DOCKER_EMAIL \
+  -n $NAMESPACE
+
+kubectl get secret $SECRET_NAME -n $NAMESPACE
