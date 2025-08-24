f99f29df051d4042d1fe74c13a158720d30a0916
“saipavan.suraparaju” <saipavan.suraparaju@gmail.com>
2025-08-24 11:52:16 +0530

    Resolved secret issue

--- create-acr-secret.sh
+++ create-acr-secret.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-
 set -e
 
 echo "DOCKER_SERVER=$DOCKER_SERVER"
 echo "DOCKER_USERNAME=$DOCKER_USERNAME"
-echo "DOCKER_PASSWORD=$DOCKER_PASSWORD"
+echo "DOCKER_PASSWORD length: ${#DOCKER_PASSWORD}"
 echo "DOCKER_EMAIL=$DOCKER_EMAIL"
 
+SECRET_NAME="acr-secret"
+NAMESPACE="default"
 
-# Create new secret
-kubectl create secret docker-registry acr-secret --docker-server="$DOCKER_SERVER" --docker-username="$DOCKER_USERNAME" --docker-password="$DOCKER_PASSWORD" --docker-email="$DOCKER_EMAIL" -n default
+# Check if the secret already exists
+if kubectl get secret "$SECRET_NAME" -n "$NAMESPACE" >/dev/null 2>&1; then
+    echo "Secret '$SECRET_NAME' already exists in namespace '$NAMESPACE'. Skipping creation."
+else
+    echo "Creating secret '$SECRET_NAME' in namespace '$NAMESPACE'..."
+    kubectl create secret docker-registry "$SECRET_NAME" --docker-server="$DOCKER_SERVER" --docker-username="$DOCKER_USERNAME" --docker-password="$DOCKER_PASSWORD" --docker-email="$DOCKER_EMAIL" -n "$NAMESPACE"
+fi
 
-kubectl get secrets -n default
+# List and describe the secret
+kubectl get secrets -n "$NAMESPACE"
+kubectl describe secret "$SECRET_NAME" -n "$NAMESPACE"
 
-kubectl describe secret acr-secret
